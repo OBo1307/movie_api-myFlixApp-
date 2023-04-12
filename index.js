a28c4ced24110a4a3c6e038dd593a52f218fab50
OBo1307 <och.bogdan@gmail.com>
2023-04-12 07:55:07 +0100

    added ethics data security
    - CORS
    - Password hashing
    - Validation logic

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// Import express, morgan, bodyParser, uuid and built in node modules fs and path
+// Import express, express-validator, morgan, bodyParser, uuid and built in node modules fs and path
 const express = require('express'),
     morgan = require('morgan'),
     bodyParser = require('body-parser'),
     uuid = require('uuid'),
     fs = require('fs'),
     path = require('path');
+const { check, validationResult } = require('express-validator');
 const { error } = require('console');
 
 const app = express();
@@ -20,6 +21,21 @@ mongoose.connect('mongodb://127.0.0.1:27017/mfDB', {
     useNewUrlParser: true, 
     useUnifiedTopology: true 
 });
+
+//CORS module and Origins
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'https://boflixapp.herokuapp.com'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn't found on the list of allowed origins 
+            let message = 'The CORS policy for this application doesn\'t allow access form origin ' + origin;
+            return callback(new Error(message ), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 // bodyParser middleware functions
 app.use(bodyParser.json());
@@ -46,31 +62,6 @@ app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) 
         .catch((err) => {
             console.error(err);
             res.status(500).send('Error: ' + err);
-        });
-});
-
-// Temporary POST(Create) a new movie
-app.post('/movies', (req, res) => {
-    Movies.findOne({ Title: req.body.Title })
-        .then((movie) => {
-            if (movie) {
-                return res.status(400).send(req.body.Title + 'already exists');
-            } else {
-                Movies.create({
-                    Title: req.body.Title,
-                    Genre: req.body.Genre,
-                    Director: req.body.Director,
-                })
-                .then((movie) => {res.status(201).json(movie)})
-                .catch((error) => {
-                    console.error(error);
-                    res.status(500).send('Error: ' + error);
-                })
-            }
-        })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).send('Error: ' + error);
         });
 });
 
@@ -122,74 +113,112 @@ app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) =
         });
 });
 
-// POST(CREATE) in Mongoose. Allow new users to register
-app.post('/users', (req, res) => {
-    Users.findOne({ Username: req.body.Username })
-        .then((user) => {
-            if (user) {
-                return res.status(400).send(req.body.Username + 'already exists');
-            } else {
-                Users.create({
-                    Username: req.body.Username,
-                    Password: req.body.Password,
-                    Email: req.body.Email,
-                    Birthday: req.body.Birthday
-                })
-                .then((user) => {res.status(201).json(user)})
-                .catch((error) => {
-                    console.error(error);
-                    res.status(500).send('Error: ' + error);
-                })
-            }
-        })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).send('Error: ' + error);
-        });
-});
+// POST(CREATE) with Mongoose and Validation logic. Allow new users to register
+app.post('/users',
+	// Validation logic here for request
+	[
+		check('Username', 'Username is required').isLength({ min: 5 }),
+		check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+		check('Password', 'Password is required.').not().isEmpty(),
+		check('Email', 'Email does not appear to be valid').isEmail(),
+	],
+	(req, res) => {
+		// check the validation object for errors
+		let errors = validationResult(req);
 
-// PUT(UPDATE) with Mongoose and JWT authentication. Allow users to update their user info (username)
-app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
-    Users.findOneAndUpdate(
-        { Username: req.params.Username }, 
-        { $set: {
-            Username: req.body.Username,
-            Password: req.body.Password,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday,
-        },
-    },
-    { new: true }, // This line makes sure that the updated document is returned
-    (err, updatedUser) => {
-        if(err) {
-            console.error(err);
-            res.status(500).send('Error: ' + err);
-        } else {
-            res.json(updatedUser);
-        }
-    });
-});
+		if (!errors.isEmpty()) {
+			return res.status(422).json({ errors: errors.array() });
+		}
+
+		let hashedPassword = Users.hashPassword(req.body.Password);
+		Users.findOne({ Username: req.body.Username })
+			.then((user) => {
+				if (user) {
+					return res.status(400).send(req.body.Username + 'already exists');
+				} else {
+					Users.create({
+						Username: req.body.Username,
+						Password: hashedPassword,
+						Email: req.body.Email,
+						Birthday: req.body.Birthday,
+					})
+						.then((user) => {
+							res.status(201).json(user);
+						})
+						.catch((error) => {
+							console.error(error);
+							res.status(500).send('Error: ' + error);
+						});
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+				res.status(500).send('Error: ' + error);
+			});
+	}
+);
+
+// PUT(UPDATE) with Mongoose, JWT authentication and Validation logic. Allow users to update their user info
+app.put('/users/:Username', passport.authenticate('jwt', { session: false }),
+	// Validation logic here for request
+	[
+		check('Username', 'Username is required').isLength({ min: 5 }),
+		check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+		check('Password', 'Password is required.').not().isEmpty(),
+		check('Email', 'Email does not appear to be valid').isEmail(),
+	],
+	(req, res) => {
+		// check the validation object for errors
+		let errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(422).json({ errors: errors.array() });
+		}
+        let hashedPassword = Users.hashPassword(req.body.Password);
+		Users.findOneAndUpdate(
+			{ Username: req.params.Username },
+			{
+				$set: {
+					Username: req.body.Username,
+					Password: hashedPassword,
+					Email: req.body.Email,
+					Birthday: req.body.Birthday,
+				},
+			},
+			{ new: true }, // This line makes sure that the updated document is returned
+			(err, updatedUser) => {
+				if (err) {
+					console.error(err);
+					res.status(500).send('Error: ' + err);
+				} else {
+					res.json(updatedUser);
+				}
+			}
+		);
+	}
+);
 
 // POST(CREATE) with Mongoose and JWT authentication. Allow users to add a movie to their list of favorites
 app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
-    Users.findOneAndUpdate(
-        { Username: req.body.Username }, 
-        { $push: { FavoriteMovies: req.params.MovieID },
-    },
-    { new: true }, // This line makes sure that the updated document is returned
-    (err, updatedUser) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Error: ' + err);
-        } else {
-            res.json(updatedUser);
-        }
-    });
-});
+		Users.findOneAndUpdate(
+			{ Username: req.params.Username },
+			{ $push: { FavoriteMovies: req.params.MovieID } },
+			{ new: true }, // This line makes sure that the updated document is returned
+			(err, updatedUser) => {
+				if (err) {
+					console.error(err);
+					res.status(500).send('Error: ' + err);
+				} else {
+					res.json(updatedUser);
+				}
+			}
+		);
+	}
+);
 
 // DELETE with Mongoose and JWT authentication. Allow users to remove a movie from their list of favorites
 app.delete('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
-    Users.findOneAndUpdate({ Username: req.body.Username }, 
+    Users.findOneAndUpdate({ Username: req.params.Username }, 
         { $pull: { FavoriteMovies: req.params.MovieID }
     },
     { new: true }, 
@@ -236,6 +265,7 @@ app.use((err, req, res, next) => {
 });
 
 // listen for requests
-app.listen(8080, () => {
-    console.log('My app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+    console.log('Listening on Port ' + port);
 });
